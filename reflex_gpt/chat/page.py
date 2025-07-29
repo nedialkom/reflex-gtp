6484c337b3d48312981e2418ea9bfa1e1ff774c7
nedialko <nedialkom@gmail.com>
2025-07-29 20:06:09 +0200

    Chat-like layout

--- reflex_gpt/chat/page.py
+++ reflex_gpt/chat/page.py
@@ -3,13 +3,41 @@ import reflex as rx
 from reflex_gpt import ui
 
 from .form import chat_form
+from .state import ChatMessage, ChatState
+
+message_style = dict(
+    display="inline-block",
+    padding="1em",
+    border_radius="8px",
+    max_width=["30em", "30em", "50em", "50em", "50em", "50em"]
+)
+
+def message_box(chat_message: ChatMessage) -> rx.Component:
+    return rx.box(
+        rx.box(
+            rx.markdown(
+                chat_message.message,
+                background_color=rx.cond(chat_message.is_bot, rx.color("mauve", 4), rx.color('blue', 4)),
+                color=rx.cond(chat_message.is_bot, rx.color("mauve", 12), rx.color('blue', 12)),
+                **message_style,
+            ),
+            text_align=rx.cond(chat_message.is_bot, "left", "right"),
+            margin_top="1em",
+        ),
+        width="100%",
+    )
 
 def chat_page():
 
     return ui.base_layout(
         rx.vstack(
             rx.heading("Chat Here", size="9"),
+            rx.box(
+                rx.foreach(ChatState.messages, message_box),
+                width="100%",
+            ),
             chat_form(),
+            margin='3rem auto',
             spacing="5",
             justify="center",
             min_height="85vh",
--- reflex_gpt/chat/state.py
+++ reflex_gpt/chat/state.py
@@ -1,18 +1,37 @@
 import asyncio
 import reflex as rx
+from typing import List
+
+
+class ChatMessage(rx.Base):
+    message: str
+    is_bot: bool = False
 
 
 class ChatState(rx.State):
     did_submit: bool = False
+    messages: List[ChatMessage] = []
 
     @rx.var
     def user_did_submit(self) -> bool:
         return self.did_submit
 
+    def append_message(self, message, is_bot:bool=False):
+        self.messages.append(
+            ChatMessage(
+                message=message,
+                is_bot=is_bot
+            )
+        )
+
     async def handle_submit(self, form_data:dict):
         print(form_data)
-        self.did_submit = True
-        yield
-        await asyncio.sleep(2)
-        self.did_submit = False
-        yield
+        user_message = form_data.get('message')
+        if user_message:
+            self.did_submit = True
+            self.append_message(user_message, is_bot=False)
+            yield
+            await asyncio.sleep(2)
+            self.did_submit = False
+            self.append_message(user_message, is_bot=True)
+            yield
